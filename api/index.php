16326594ef3fbb6abcbe15603206663615b41584
Ariansyah Putra <52442252+FuckHeart@users.noreply.github.com>
2026-01-07 15:56:19 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
 <title>Sign in with Apple Account</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="/assets/style.css">
 
 </head>
 
